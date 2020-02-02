a0a82e0d6d0aba0559e5b0e8c81e1c616974d5e8
cdugga <duggan.colin@gmail.com>
2020-02-02 20:28:12 +0000

    gen csr

--- py_gen_cert.py
+++ py_gen_cert.py
@@ -1,7 +1,34 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+
 import sys
+
+
+def generate_csr(key):
+    csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+        # Provide various details about who we are.
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
+    ])).add_extension(
+        x509.SubjectAlternativeName([
+        # Describe what sites we want this certificate for.
+            x509.DNSName(u"mysite.com"),
+            x509.DNSName(u"www.mysite.com"),
+            x509.DNSName(u"subdomain.mysite.com"),
+        ]),
+        critical=False,
+    # Sign the CSR with our private key.
+    ).sign(key, hashes.SHA256(), default_backend())
+    # Write our CSR out to disk.
+    with open("./csr.pem", "wb") as f:
+        f.write(csr.public_bytes(serialization.Encoding.PEM))
 
 def generate_key():
     key = rsa.generate_private_key(
@@ -16,11 +43,15 @@ def generate_key():
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.BestAvailableEncryption(b"passphrase"),
         ))
+    return key
 
 def main():
     print("Generating key...")
-    generate_key
+    key = generate_key()
     print("Done..")
+    print("Generate CSR")
+    generate_csr(key)
+    print("Done")
 
 if __name__ == '__main__':
     main()
